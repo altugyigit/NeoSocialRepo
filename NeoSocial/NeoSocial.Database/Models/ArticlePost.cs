ed08f6dbe097b7c7dd284404f9c09a2206e075aa
mertkozcan <mertkozcan@outlook.com>
2015-08-05 17:08:08 +0300

    Mapping yapıldı

---
+++ NeoSocial/NeoSocial.Database/Models/ArticlePost.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeoSocial.Database.Models
+{
+   public partial class ArticlePost
+
+
+    {
+
+       public ArticlePost() {
+
+           this.Post_Like_Count = 0;
+   
+   
+   }
+
+
+
+        public int PostID { get; set; }
+        public string PostHeader { get; set; }
+        public string PostContent { get; set; }
+        public string PostDate { get; set; }
+        public int PostLikeCount { get; set; }
+        public int PostCommentID { get; set; }
+        public int PostOwnerID { get; set; }
+    }
+}
