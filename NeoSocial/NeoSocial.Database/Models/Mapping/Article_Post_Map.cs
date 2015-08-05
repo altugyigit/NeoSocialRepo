2a60dbbf6477630135f50c9b8d79abe2748ff38e
mertkozcan <mertkozcan@outlook.com>
2015-08-05 16:29:15 +0300

    Database models ve mapping eklendi

---
+++ NeoSocial/NeoSocial.Database/Models/Mapping/Article_Post_Map.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+
+namespace NeoSocial.Database.Models.Mapping
+{
+    class Article_Post_Map : EntityTypeConfiguration<Article_Post>
+    {
+        public Article_Post_Map()
+        {
+            
+        }
+    }
+}
