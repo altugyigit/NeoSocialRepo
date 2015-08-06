2b8a0e5f13f8260751660a67261083851d8e3f6a
altugyigit <altug_yigit@hotmail.com>
2015-08-06 11:22:37 +0300

    Rev5

---
+++ NeoSocial/NeoSocial.Database/Models/DB_9D589F_neosocialContext.cs
@@ -0,0 +1,28 @@
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using NeoSocial.Database.Models.Mapping;
+
+namespace NeoSocial.Database.Models
+{
+    public partial class DB_9D589F_neosocialContext : DbContext
+    {
+        static DB_9D589F_neosocialContext()
+        {
+            System.Data.Entity.Database.SetInitializer<DB_9D589F_neosocialContext>(null);
+        }
+
+        public DB_9D589F_neosocialContext()
+            : base("Name=DB_9D589F_neosocialContext")
+        {
+        }
+
+        public DbSet<ArticlePost> ArticlePosts { get; set; }
+        public DbSet<DENEME> DENEMEs { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new ArticlePostMap());
+            modelBuilder.Configurations.Add(new DENEMEMap());
+        }
+    }
+}
---
+++ NeoSocial/NeoSocial.Database/Models/DENEME.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+
+namespace NeoSocial.Database.Models
+{
+    public partial class DENEME
+    {
+        public int ID { get; set; }
+    }
+}
