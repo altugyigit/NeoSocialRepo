b0a5608430c0c943754c7e7f25922b8b4e7dcb9e
mertkozcan <mertkozcan@outlook.com>
2015-08-06 10:01:49 +0300

    mapping yapıldı

--- NeoSocial/NeoSocial.Database/Models/ArticlePost.cs
+++ NeoSocial/NeoSocial.Database/Models/ArticlePost.cs
@@ -1,31 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace NeoSocial.Database.Models
 {
-   public partial class ArticlePost
-
-
+    public partial class ArticlePost
     {
-
-       public ArticlePost() {
-
-           this.Post_Like_Count = 0;
-   
-   
-   }
-
-
-
-        public int PostID { get; set; }
+        public long PostID { get; set; }
         public string PostHeader { get; set; }
         public string PostContent { get; set; }
         public string PostDate { get; set; }
-        public int PostLikeCount { get; set; }
-        public int PostCommentID { get; set; }
-        public int PostOwnerID { get; set; }
+        public Nullable<int> PostLikeCount { get; set; }
+        public Nullable<int> PostCommentID { get; set; }
+        public Nullable<int> PostOwnerID { get; set; }
     }
 }
--- NeoSocial/NeoSocial.Database/Models/Mapping/ArticlePostMap.cs
+++ NeoSocial/NeoSocial.Database/Models/Mapping/ArticlePostMap.cs
@@ -1,53 +1,36 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity.ModelConfiguration;
 
 namespace NeoSocial.Database.Models.Mapping
 {
-    class ArticlePostMap : EntityTypeConfiguration<ArticlePost>
+    public class ArticlePostMap : EntityTypeConfiguration<ArticlePost>
     {
         public ArticlePostMap()
         {
-            //primary key
-            this.HasKey(x => new { x.PostID,x.PostOwnerID,x.PostCommentID });
+            // Primary Key
+            this.HasKey(t => t.PostID);
 
-            //property
-            this.Property(x => x.PostID)
-                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
-
-            this.Property(x=> x.PostOwnerID)
-                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
-
-            this.Property(x => x.PostCommentID)
-                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.None);
-
-            this.Property(x => x.PostHeader)
-                .IsOptional()
+            // Properties
+            this.Property(t => t.PostHeader)
                 .HasMaxLength(200);
 
-            this.Property(x => x.PostContent)
+            this.Property(t => t.PostContent)
                 .IsRequired()
                 .HasMaxLength(2000);
 
-            this.Property(x => x.PostDate)
+            this.Property(t => t.PostDate)
                 .IsRequired()
-                .HasMaxLength(100);
+                .HasMaxLength(50);
 
-            this.Property(x => x.PostLikeCount)
-                .IsOptional();
-                
-            //table&column
-
-            this.ToTable("Article_Post");
-            this.Property(x => x.PostID).HasColumnName("PostID");
-              this.Property(x => x.PostHeader).HasColumnName("PostHeader");
-            this.Property(x => x.PostContent).HasColumnName("PostContent");
-            this.Property(x => x.PostDate).HasColumnName("PostDate");
-            this.Property(x => x.PostLikeCount).HasColumnName("PostLikeCount");
-            this.Property(x => x.PostCommentID).HasColumnName("PostCommentID");
-            this.Property(x => x.PostOwnerID).HasColumnName("PostOwnerID");
-          
-
-
+            // Table & Column Mappings
+            this.ToTable("ArticlePost");
+            this.Property(t => t.PostID).HasColumnName("PostID");
+            this.Property(t => t.PostHeader).HasColumnName("PostHeader");
+            this.Property(t => t.PostContent).HasColumnName("PostContent");
+            this.Property(t => t.PostDate).HasColumnName("PostDate");
+            this.Property(t => t.PostLikeCount).HasColumnName("PostLikeCount");
+            this.Property(t => t.PostCommentID).HasColumnName("PostCommentID");
+            this.Property(t => t.PostOwnerID).HasColumnName("PostOwnerID");
         }
     }
 }
