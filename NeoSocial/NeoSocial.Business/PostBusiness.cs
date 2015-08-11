ded54e10cfba917db8ba9aa779d17fbdfc43dcc7
altugyigit <altug_yigit@hotmail.com>
2015-08-11 10:31:51 +0300

    Post Business eklendi.

---
+++ NeoSocial/NeoSocial.Business/PostBusiness.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeoSocial.Database.Models;
+using NeoSocial.Database.Repository;
+using NeoSocial.Database.IUnitOfWork;
+
+namespace NeoSocial.Business
+{
+    interface IPostBusiness
+    {
+        List<ArticlePost> getAllArticlePost();
+        List<ArticlePost> getUserArticlePost();
+    }
+    public class PostBusiness : IPostBusiness
+    {
+        PostContext _postContext;
+
+        public PostBusiness()
+        {
+
+            _postContext = new PostContext(new DbContextFactory());   
+
+        }
+        public List<ArticlePost> getAllArticlePost()
+        {
+             ArticlePost _articlePost = _postContext.ArticlePostRepository.Get().First();
+
+             List<ArticlePost> _articlePostList = new List<ArticlePost>();
+
+             _articlePostList.Add(_articlePost);
+
+             return _articlePostList;
+        }
+    }
+}
