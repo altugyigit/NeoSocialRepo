0ba41305fcb4073a957f2b3ecbf492119a4bb9d3
altugyigit <altug_yigit@hotmail.com>
2015-08-11 14:35:36 +0300

    Post Listeleme Oldu.

--- NeoSocial/NeoSocial/Controllers/MainController.cs
+++ NeoSocial/NeoSocial/Controllers/MainController.cs
@@ -1,5 +1,4 @@
-﻿using NeoSocial.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,18 +13,8 @@ namespace NeoSocial.Controllers
         //
         // GET: /Main/
 
-        public CommonPost articlePost = new CommonPost();
-
         public ActionResult MainPage()
-        {
-            List<CommonPost> postList = new List<CommonPost>();
-
-            postList.Add(new CommonPost() { PostID = 0, PostHeader="Güzel bir gün.", PostContent="Bugün çok mutluyum. :)", PostDate="10/08/2015", PostCommentID=0, PostLikeCount=0});
-            postList.Add(new CommonPost() { PostID = 0, PostHeader = "Berbat bir gün.", PostContent = "Allah kahretsin.", PostDate = "10/08/2015", PostCommentID = 0, PostLikeCount = 0 });
-            postList.Add(new CommonPost() { PostID = 0, PostHeader = "Hava güzel olunca", PostContent = "Hava güzel olunca piknik kaçmaz. :)", PostDate = "10/08/2015", PostCommentID = 0, PostLikeCount = 0 });
-
-            ViewData["postList"] = postList;
-
+        {            
             PostBusiness _postBusiness = new PostBusiness();
             ViewData["postDatabase"] = _postBusiness.getAllArticlePost();
 
