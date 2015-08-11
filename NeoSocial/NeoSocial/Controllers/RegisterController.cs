df0f4c98c5da4d9e60ffa56af1009f90e8dd0dc8
mertkozcan <mertkozcan@outlook.com>
2015-08-11 14:38:13 +0300

    komm

--- NeoSocial/NeoSocial/Controllers/RegisterController.cs
+++ NeoSocial/NeoSocial/Controllers/RegisterController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NeoSocial.Database.Models;
+using NeoSocial.Database.Repository;
+using NeoSocial.Database.IUnitOfWork;
+using NeoSocial.Business;
 
 namespace NeoSocial.Controllers
 {
@@ -10,10 +14,34 @@ namespace NeoSocial.Controllers
     {
         //
         // GET: /Register/
+        RegisterBusiness b = new RegisterBusiness();
+        
 
         public ActionResult Register()
         {
             return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult CreateUser(UserRegister model)
+        {
+            if (model.UserRegisterID == 0)
+            {
+                b.InsertUser(model);
+              
+            }
+            else
+            {
+               
+                
+            }
+
+            if (model.UserRegisterID > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(model);
         }
 
     }
