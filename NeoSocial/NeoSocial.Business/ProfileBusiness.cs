ea86ee7e2247e00731eff9f187818fc7f9a875c5
altugyigit <altug_yigit@hotmail.com>
2015-08-21 17:10:34 +0300

    ProfileBusiness

---
+++ NeoSocial/NeoSocial.Business/ProfileBusiness.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeoSocial.Database.Models;
+using NeoSocial.Database.IUnitOfWork;
+using NeoSocial.Database.Repository;
+
+namespace NeoSocial.Business
+{
+    interface IProfileBusiness
+    {
+        UserProfile getProfileInfo();
+    }
+    public class ProfileBusiness:IProfileBusiness
+    {
+        UserContext _userContext;
+        UserProfile _userProfile;
+
+        public ProfileBusiness()
+        {
+            _userContext = new UserContext(new DbContextFactory());
+        }
+
+        public UserProfile getProfileInfo(int userId)
+        {
+            _userProfile = new UserProfile();
+
+            _userContext.UserProfileRepository.Find(a => a.UserID == userId);
+
+            return _userProfile; 
+        }
+    }
+}
